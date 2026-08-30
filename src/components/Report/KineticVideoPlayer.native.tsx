0b563fad616e59aafbf551a2f10d84e7ab852b5b
bjgeddes17-ux <bjgeddes17@gmail.com>
2026-08-30 08:41:58 +0200

    Gijghh

--- src/components/Report/KineticVideoPlayer.native.tsx
+++ src/components/Report/KineticVideoPlayer.native.tsx
@@ -178,10 +178,18 @@ const styles = StyleSheet.create({
     position: 'relative',
   },
   video: {
-    ...StyleSheet.absoluteFill,
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
   },
   canvas: {
-    ...StyleSheet.absoluteFill,
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
     zIndex: 10,
   },
 });
